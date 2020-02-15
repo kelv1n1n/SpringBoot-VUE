d5bfa7c72f2bdd751a4d78d4c81aa920e8c58f37
Ctrl08 <390022566@qq.com>
2020-02-15 11:26:57 +0800

    创建vue项目并添加book页面

---
+++ vue_demo/src/views/Book.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <tr>
+          <td>编号</td>
+          <td>图书名称</td>
+          <td>作者</td>
+        </tr>
+      </thead>
+      <tbody>
+          <!-- {{msg}} -->
+        <tr v-for="item in books" v-bind:key="item">
+          <!-- <td>{{books[0].id}}</td>
+          <td>{{books[0].name}}</td>
+          <td>{{books[0].author}}</td> -->
+          <td>{{item.id}}</td>
+          <td>{{item.name}}</td>
+          <td>{{item.author}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Book",
+    data(){
+        return {
+            msg:"Hello,vue",
+            books:[
+                {
+                    id:1,
+                    name:'Java',
+                    author:'Lee'
+                },
+                {
+                    id:1,
+                    name:'Vue',
+                    author:'Lee'
+                },
+                {
+                    id:1,
+                    name:'Springboot',
+                    author:'Lee'
+                }
+            ]
+        }
+    }
+};
+</script>
+
+
+<style>
+</style>
